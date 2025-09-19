f7c6cab9c09c2f4721fc8cc4c20175cbf14d00b7
ígorpetriv.bsc <igorpetriv.bsc@gmail.com>
2025-09-19 20:29:13 +0300

    styles delete

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,3 @@
 export default function DashboardPage() {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100%',
-      }}
-    ></div>
-  );
+  return <div></div>;
 }
