5c32bb0092e6dcdc7b4bdd4ec437cf5508b52a1c
Irina Ozerova <marta.ilyashenko@gmail.com>
2025-09-23 16:12:45 +0300

    add route auth in new branch

---
+++ lib/api/clientApi.ts
@@ -0,0 +1,49 @@
+import { serverApi } from './api';
+import {
+  RegisterRequest,
+  User,
+  CheckSessionRequest,
+  LoginRequest,
+} from '@/types/user';
+
+// REGISTER
+
+export const register = async (data: RegisterRequest) => {
+  const res = await serverApi.post<User>('/auth/register', data);
+  return res.data;
+};
+
+// LOGIN
+
+export const login = async (data: LoginRequest) => {
+  const res = await serverApi.post<User>('/auth/login', data);
+  return res.data;
+};
+
+// LOGOUT
+
+export const logout = async (): Promise<void> => {
+  await serverApi.post('/auth/logout');
+};
+
+// AUTH ME
+
+export const getMe = async (): Promise<User> => {
+  const { data } = await serverApi.get('/users/me', {
+    withCredentials: true,
+  });
+
+  return data;
+};
+
+// CHECK SESSION
+
+export const checkSession = async () => {
+  const res = await serverApi.get<CheckSessionRequest>('/auth/session', {
+    withCredentials: true,
+  });
+
+  console.log('Session response:', res.data);
+
+  return res.data.success;
+};
