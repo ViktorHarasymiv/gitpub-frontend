2b879daf05e9bddbd6d9887050486dc853111319
Irina Ozerova <marta.ilyashenko@gmail.com>
2025-09-23 12:49:52 +0300

    find bug

--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -1,6 +1,6 @@
 import { cookies } from 'next/headers';
 import nextServer from './api';
-import { ServerBoolResponse, User } from '@/types/user';
+import { ServerBoolResponse } from '@/types/user';
 
 // session
 export const checkServerSession = async () => {
@@ -9,13 +9,4 @@ export const checkServerSession = async () => {
     headers: { Cookie: cookieData.toString() },
   });
   return response;
-};
-
-//getMe
-export const getServerMe = async (): Promise<User> => {
-  const cookieData = await cookies();
-  const { data } = await nextServer.get(`/users/me`, {
-    headers: { Cookie: cookieData.toString() },
-  });
-  return data;
 };
