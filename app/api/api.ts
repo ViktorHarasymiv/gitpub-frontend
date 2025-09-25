49ffef37db39630ae32d7034256c1520428b3a39
Viktor <victorharasymiv97@gmail.com>
2025-09-25 15:59:11 +0200

    New update 25

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BEKEND_ORIGIN,
+  // baseURL: 'https://gitpub-backend-qq3d.onrender.com',
+  baseURL: 'http://localhost:4000',
   withCredentials: true,
 });
