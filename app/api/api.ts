446813a38b4de7b1ac00ef693034959f2473e8e6
Viktor <victorharasymiv97@gmail.com>
2025-09-25 21:13:24 +0200

    New api keys

--- app/api/api.ts
+++ app/api/api.ts
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BEKEND_ORIGIN,
+  // baseURL: 'http://localhost:4000',
   withCredentials: true,
 });
