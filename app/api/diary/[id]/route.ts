38deae4d50a47a140e4410f27e5bdfd414e34841
Denys Tkachenko <sinedtex@gmail.com>
2025-09-26 21:55:58 +0300

    fix route

--- app/api/diary/[id]/route.ts
+++ app/api/diary/[id]/route.ts
@@ -1,15 +1,16 @@
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 import { api } from '../../api';
 import { cookies } from 'next/headers';
 
 type Props = {
-  params: Promise<{ _id: string }>;
+  params: Promise<{ id: string }>;
 };
-export async function GET(request: Request, { params }: Props) {
+
+export async function GET(request: NextRequest, { params }: Props) {
+  const { id } = await params;
   const cookieStore = await cookies();
   const accessToken = cookieStore.get('accessToken')?.value;
-  const { _id } = await params;
-  const resp = await api(`/diaries/${_id}`, {
+  const resp = await api(`/diaries/${id}`, {
     headers: {
       Authorization: `Bearer ${accessToken}`,
       //   Cookie: cookieStore.toString(),
@@ -26,13 +27,13 @@ export async function GET(request: Request, { params }: Props) {
   );
 }
 
-export async function DELETE(request: Request, { params }: Props) {
+export async function DELETE(request: NextRequest, { params }: Props) {
   const cookieStore = await cookies();
   const accessToken = cookieStore.get('accessToken')?.value;
-  const { _id } = await params;
+  const { id } = await params;
 
   try {
-    await api.delete(`/diaries/${_id}`, {
+    await api.delete(`/diaries/${id}`, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
         'Content-Type': 'application/json',
@@ -52,15 +53,15 @@ export async function DELETE(request: Request, { params }: Props) {
   }
 }
 
-export async function PATCH(request: Request, { params }: Props) {
+export async function PATCH(request: NextRequest, { params }: Props) {
   const cookieStore = await cookies();
   const accessToken = cookieStore.get('accessToken')?.value;
-  const { _id } = await params;
+  const { id } = await params;
 
   try {
     const body = await request.json();
 
-    const resp = await api.patch(`/diaries/${_id}`, body, {
+    const resp = await api.patch(`/diaries/${id}`, body, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
         //   Cookie: cookieStore.toString(),
@@ -70,7 +71,7 @@ export async function PATCH(request: Request, { params }: Props) {
     return NextResponse.json(resp.data, { status: 200 });
   } catch (error) {
     return NextResponse.json(
-      { error: 'Failed to update diary' },
+      { error: `Failed to update diary, ${error}` },
       { status: 500 }
     );
   }
